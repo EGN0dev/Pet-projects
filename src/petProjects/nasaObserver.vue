bd23fe8b4161a382cedc1c0d6dd47531e7c20b12
Eugene <dubr.troll@gmail.com>
2023-12-29 03:45:53 +0300

    nasaObserver app created, development in progress...

--- src/petProjects/nasaObserver.vue
+++ src/petProjects/nasaObserver.vue
@@ -3,53 +3,24 @@ import { defineComponent,ref } from 'vue';
 
 export default defineComponent({
     setup() {
-        const imagePreview = ref<string>('image-preview')
-        const imagePreviewBool = ref<boolean>(false)
-        const imageDataArray = ref<string[]>([])
-        const imageData = ref<string>("")
-        const imageGalleryElement = ref<string>("images") 
-        const index0fActive = ref<number>()
-        
+       
+        const imageData = ref<string>("https://www.memetemplates.in/uploads/1640209265.jpeg")
+      
 
-        const onDragOver = (event:any) => {
-            event.preventDefault()
-            event.dataTransfer.dropEffect = "copy"
-        }
+        let url = 'https://api.nasa.gov/planetary/apod?api_key=1C6088ZVNYInCaU0ldb3icmedNhci1myzg27lO8w'
 
-        const onDrop = (event:any) => {
-            event.preventDefault()
-            let files = event.dataTransfer.files
-            for (let i = 0; i < files.length; i++){
-            imageDataArray.value.push(URL.createObjectURL(files[i])) 
-           }
-        }
-        
-        const changePic = (index:number) =>{
-            imagePreviewBool.value = true
-            imageData.value=imageDataArray.value[index]
-            imageGalleryElement.value='imagesSelected'
-            index0fActive.value = index 
-        }
+            let image = ''
 
-        const previewImage = (event:any) => {
-                var input = event.target;
-                     // Ensure that you have a file before attempting to read it
-                         if (input.files && input.files[0]) {
-                                 // create a new FileReader to read this image and convert to base64 format
-                                 var reader = new FileReader();
-                                    // Define a callback function to run, when FileReader finishes its job
-                                    reader.onload = (e) => {
-                                        // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
-                                            // Read image as base64 and set to imageData
-                                               // imageData.value = e.target.result;
-                                                let stringHolder:string = e.target.result;
-                                                    imageDataArray.value.push(stringHolder)}
-                                                    // Start the reader job - read file as a data url (base64 format)
-                                                      reader.readAsDataURL(input.files[0]);
-            }
-        }
+            const fetc = async() =>{
+
+            await fetch(url).then(url=> url.json()).then(url=> imageData.value = url.url)
+
+            console.log(image)
             
-        return {onDrop,onDragOver,previewImage,changePic,imageData,imageDataArray,imageGalleryElement,index0fActive,imagePreview,imagePreviewBool}
+            }
+
+            
+        return {imageData,fetc}
 
         }
 })
@@ -59,22 +30,11 @@ export default defineComponent({
     <template>
      
         <div class="main">
-                    <div class="file-upload-form"  @dragover.prevent="onDragOver" @drop.prevent="onDrop">
-                        Drag an image file here or:
-                            <input class="upload_btn" type="file" @change="previewImage" accept="image/*">
-                                <div class="overlay-layer">Upload photo</div>
-                    </div>
-                    <div class="bodyGallery" v-if="imageDataArray.length != 0">
-                        <div class="image-preview" v-if="imagePreviewBool" >
+            <button @click="fetc"> press me </button>
+               
+                    <div class="bodyGallery">
+                        <div class="image-preview">
                             <img class="preview" :src="imageData">
-                        </div>
-                        <div class="imagePreviewHide" v-else="!imagePreviewBool" >
-                            <span>Choose picture below</span>
-                        </div>
-                        <div class="imagesContainer">
-                            <div v-for="(image,index) in imageDataArray"> 
-                                <img :class="(index==index0fActive) ? imageGalleryElement='imagesSelected' :  imageGalleryElement='images'"  :src="image" @click="changePic(index)"/>
-                            </div>
                         </div>
                     </div>
         </div>
